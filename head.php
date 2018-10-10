0c75d795b88ae88f915092c8d17e6195a124cca0
Gaurav prajapati <36500231+jamesprajapati@users.noreply.github.com>
2018-10-11 04:44:55 +0530

    Delete head.php

--- head.php
+++
@@ -1,8 +0,0 @@
-<title>GiftShop</title>
-<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!--bootstrap css-->
- <link href="css/bootstrap.min.css" rel="stylesheet">
- <script src="js/jquery.min.js" type="text/javascript"></script>
- <script src="js/bootstrap.min.js" type="text/javascript"></script>
-<link rel="stylesheet" type="text/css" href="css/mystyle.css">
