22749832dd257fe2be5f994a24a8cfbcb58f40c0
Gaurav prajapati <36500231+jamesprajapati@users.noreply.github.com>
2018-10-12 06:11:53 +0530

    Add files via upload

--- js/mystyle.js
+++ js/mystyle.js
@@ -1,10 +1,17 @@
-function hide(){
-    var addstock = document.getElementById("stock1");
-    var updatestock =document.getElementById("stock2");
-    var frame1=document.getElementById("f1");
-    var frame2=document.getElementById("f2");
-    frame1.style.display=updatestock.checked ? "none": "block";
-   frame2.style.display=addstock.checked ? "none" : "block";
+function hide()
+          {
+            
+            var addstock = document.getElementById("stock1");
+            var updatestock =document.getElementById("stock2");
+            var frame1=document.getElementById("f1");
+            var frame2=document.getElementById("f2");      
+            if(flag==1)
+            {     flag=0;
+                  updatestock.checked=true;
+                     
+            }
+            frame1.style.display=updatestock.checked ? "none": "block";
+            frame2.style.display=addstock.checked ? "none" : "block";    
 
-
-}
+            
+          }
